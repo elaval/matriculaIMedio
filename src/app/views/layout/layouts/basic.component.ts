649fcb7bea67640b5b8605da3fef1faef6a716f2
Ernesto Laval <ernesto.laval@gmail.com>
2017-07-03 02:27:23 +0100

    Travis node 6.10

--- src/app/views/layout/layouts/basic.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-declare var jQuery:any;
-
-@Component({
-    selector: 'basic_',
-    templateUrl: 'basic.template.html'
-})
-export class basicComponent {}
